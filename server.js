223a6a2bc3d8285b96d3d63f13b2007d8551dece
tettehq <ronyquash@gmail.com>
2023-01-14 23:53:07 +0000

    week 2 personal assignment

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-app.use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+  mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+  });
