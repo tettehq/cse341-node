6a37fb91b11b0dafb504c55492d7bc392d678225
tettehq <ronyquash@gmail.com>
2023-01-25 16:19:30 +0000

    swagger for render

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const cors = require('cors');
 
+
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(cors());
-
 app
+  .use(cors())
   .use(bodyParser.json())
+  .use(express.urlencoded({ extended: true }))
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
